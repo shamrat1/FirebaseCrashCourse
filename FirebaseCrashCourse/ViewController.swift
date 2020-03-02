92def034f2f7bda56396fefcd55f9a508937f98a
Yasin Shamrat <yshamrat@gmail.com>
2020-03-01 20:46:22 -0800

    Initial Commit

---
+++ FirebaseCrashCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseCrashCourse
+//
+//  Created by Mac on 3/1/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
