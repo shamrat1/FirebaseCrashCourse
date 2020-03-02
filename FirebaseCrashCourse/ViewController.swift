f66c87601d2f4faa80ae30077fa570faaaf19afb
Yasin Shamrat <yshamrat@gmail.com>
2020-03-01 22:41:03 -0800

    firebase added & basic operation explored

--- FirebaseCrashCourse/ViewController.swift
+++ FirebaseCrashCourse/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let dbRef = Database.database().reference()
+//        dbRef.childByAutoId().setValue([
+//            "name":"Yasin Shamrat",
+//            "email":"yshamrat@gmail.com",
+//            "age":24
+//        ])
+        dbRef.child("-M1Ojlvj4F_cUTS3Dvpu").observe(.value, with: { (snap) in
+            print(snap.value!)
+        })
+        dbRef.observe(.value) { (snapshot) in
+            print(snapshot.value!)
+        }
     }
 
 
